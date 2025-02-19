7d58ed3174cb85abaaf0078a19fb3c47f5e66671
Arsalan Khan <arsalanazhar2003@gmail.com>
2025-02-19 06:54:07 +0500

    chat-qa done

---
+++ QA-Chat/app.py
@@ -0,0 +1,27 @@
+from groq import Groq
+from langchain_core.prompts import ChatPromptTemplate
+import os
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+Groq_api_key=os.getenv("GROQ_API_KEY")
+
+prompts=ChatPromptTemplate.from_messages([
+    ("system","Please read the question carefully and answer based on exact data"),
+    ("user","Question:{question}")
+])
+
+groq_llm=ChatGroq(model="llama-3.3-70b-versatile",groq_api_key=Groq_api_key,temperature=0.9)
+
+st.title("Arsalan Chatbot")
+input_text=st.text_input("Enter you want to search here")
+
+str_output_parser=StrOutputParser()
+
+chain=prompts|groq_llm|str_output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
